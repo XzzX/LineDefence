f6e3d503b01de5a34b1834e5692a14a911cc318f
Sebastian Eibl <sebastian.eibl@fau.de>
2021-02-19 15:59:04 +0100

    [ADD] basic CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include "Hero.h"
 
+#include "CLI/CLI.hpp"
+
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-    auto heros = YAML::LoadFile("heros.yaml");
+    CLI::App app{"App description"};
+
+    std::string filename = "heros.yaml";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
+
+    auto heros = YAML::LoadFile(filename);
     auto h = create_hero(heros);
     std::cout << h.id << std::endl;
 }
